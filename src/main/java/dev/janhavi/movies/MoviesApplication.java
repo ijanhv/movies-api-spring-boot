e2009b7ddd9152e9fee1d4b3ebcf44d13991023b
Janhavi Patil <patil.janhavi1207@gmail.com>
2023-08-27 20:03:57 +0530

    intial spring boot setup

---
+++ src/main/java/dev/janhavi/movies/MoviesApplication.java
@@ -0,0 +1,14 @@
+package dev.janhavi.movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesApplication {
+
+	public static void main(String[] args) {
+		System.out.print("HELOOOOOO");
+		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+}

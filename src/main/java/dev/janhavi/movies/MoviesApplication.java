4b501e69ad63bde70e44984fa494223f2e3909bc
Janhavi Patil <patil.janhavi1207@gmail.com>
2023-08-27 22:51:13 +0530

    complete movies and reviews api

--- src/main/java/dev/janhavi/movies/MoviesApplication.java
+++ src/main/java/dev/janhavi/movies/MoviesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class MoviesApplication {
 
 	public static void main(String[] args) {
-		System.out.print("HELOOOOOO");
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
